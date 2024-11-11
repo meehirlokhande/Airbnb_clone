1793d935cc03b1b4135d349d4ba686b91649f383
meehirlokhande <meehirlokhande@gmail.com>
2024-11-11 23:09:11 +0530

    fixed few bugs

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -2,47 +2,47 @@ import { useContext, useState } from "react";
 import { Navigate, useParams } from "react-router-dom";
 import { UserContext } from "../UserContext";
 import axios from "axios";
-import PlacesPage from "./placesPage";
+import PlacesPage from "./PlacesPage";
 import AccountNav from "../AccountNav";
 
-const  AccountPage=()=>{
-    const [redirect,setRedirect] = useState(null);
-    const {user,ready,setUser} = useContext(UserContext);
-    let {subpage}= useParams();
+const AccountPage = () => {
+    const [redirect, setRedirect] = useState(null);
+    const { user, ready, setUser } = useContext(UserContext);
+    let { subpage } = useParams();
 
-    if(!ready){
+    if (!ready) {
         return 'loading...'
     }
-    if(ready && !user && !redirect){
-        return <Navigate to = {'/login'} />
+    if (ready && !user && !redirect) {
+        return <Navigate to={'/login'} />
     }
-    
-    if(subpage=== undefined){
+
+    if (subpage === undefined) {
         subpage = 'profile';
     }
 
-    async function logout(){
+    async function logout() {
         await axios.post('/logout');
         setRedirect('/');
         setUser(null);
-        
+
     }
 
-  
-    if(redirect){
-        return <Navigate to = {redirect} />
+
+    if (redirect) {
+        return <Navigate to={redirect} />
     }
-    return(
+    return (
         <div>
-            <AccountNav/>
-            {subpage==='profile' && (
+            <AccountNav />
+            {subpage === 'profile' && (
                 <div className="text-center max-w-lg mx-auto ">
-                    Logged in as {user.name} ({user.email})<br/>
+                    Logged in as {user.name} ({user.email})<br />
                     <button onClick={logout} className="bg-primary text-white max-w-sm  mt-2 py-2 px-4 rounded-full">Logout</button>
                 </div>
             )}
-            {subpage==='places' && (
-                <PlacesPage/>
+            {subpage === 'places' && (
+                <PlacesPage />
             )}
         </div>
     );
